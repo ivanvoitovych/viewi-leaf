fe036e65a1ce5b20fd1e54860769f7cf3550f6ff
Ivan Voitovych <col606v@gmail.com>
2022-08-12 12:48:15 +0300

    api posts endpoint

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Components\Models\PostModel;
 use Components\Views\Home\HomePage;
 use Components\Views\NotFound\NotFoundPage;
 use Components\Views\Pages\CounterPage;
@@ -16,6 +17,14 @@ viewi()->get('*', NotFoundPage::class);
 
 app()->get("/api/data", function () {
     response()->json(["message" => "Hello World!"]);
+});
+
+app()->get("/api/posts/{id}", function ($id) {
+    $post = new PostModel();
+    $post->Id = $id ?? 0;
+    $post->Name = 'Viewi ft. Leaf';
+    $post->Version = 1;
+    response()->json($post);
 });
 
 app()->run();
